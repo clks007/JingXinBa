1b3eefbbddd12773750ee7ee7682be99560d39d1
zerostart <zg15074753344@163.com>
2016-01-17 00:05:10 +0800

    111111111111

--- recycleviewdemo/src/main/java/com/xk/recycleviewdemo/MainActivity.java
+++ recycleviewdemo/src/main/java/com/xk/recycleviewdemo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView rc;
+        new Thread();
     }
 
     @Override
